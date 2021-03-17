aee9865c414ad88b7d7217c18474baddb05cba6e
techtorial <50930535+techtorial@users.noreply.github.com>
2021-03-16 19:03:05 -0500

    Test
    Test

--- src/main/java/GeitTest.java
+++ src/main/java/GeitTest.java
@@ -4,5 +4,6 @@ public class GeitTest {
         System.out.println("100_000.00$");
         System.out.println("100");
         System.out.println("Amir");
+        System.out.println("100");
     }
 }
